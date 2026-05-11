6d58c8f1556e29d85070bdc244ef815576ba8bd5
Raphael Maciel Vilela <raph.mv.cbjr@gmail.com>
2026-05-11 14:30:47 -0300

    HandlerException

--- src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package dio.web.api.repository;
 
+import dio.web.api.handler.BusinessException;
+import dio.web.api.handler.CampoObrigatorioException;
 import dio.web.api.model.Usuario;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,12 @@ import java.util.List;
 public class UsuarioRepository {
 
     public void save(Usuario usuario){
+
+        if(usuario.getLogin()==null)
+            throw new CampoObrigatorioException("login");
+        if(usuario.getPassword()==null)
+            throw new CampoObrigatorioException("password");
+
         if(usuario.getId()==null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
